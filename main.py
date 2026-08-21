8d33bbdad01c4dc08a5689fd8ca8238cfe791568
Andy <andreahenriquez2705@gmail.com>
2026-08-21 15:18:42 -0300

    Refactor main.py to organize environment variable loading and Discord client setup

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 from dotenv import load_dotenv
+import os
+import discord
+
+
+#-------------------------------------------------------#
+#              LOAD ENVIRONMENT VARIABLES               #
+#-------------------------------------------------------#
+
+load_dotenv()  # reads variables from a .env file and sets them in os.environ
+
+#Carga de token, conexión con la API de Discord
+token = os.getenv("DISCORD_TOKEN")
+
+#-------------------------------------------------------#
+#                       CLIENT DISCORD                  #
+#-------------------------------------------------------#
+
+intents=discord.Intents.default()
