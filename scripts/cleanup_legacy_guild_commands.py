5e78d8659a5ffb654e5c1fa01ff88bece999f803
Andy <andreahenriquez2705@gmail.com>
2026-08-28 16:35:12 -0300

    chore: add guild command cleanup utility

--- scripts/cleanup_legacy_guild_commands.py
+++ scripts/cleanup_legacy_guild_commands.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 
 
 OBSOLETE_COMMANDS = {"market", "ping"}
+PUBLIC_GLOBAL_COMMANDS = {"sprint", "profile", "config", "help"}
 
 
 def parse_arguments():
@@ -20,10 +21,21 @@ def parse_arguments():
         required=True,
         help="Discord guild ID whose legacy commands should be removed."
     )
+    parser.add_argument(
+        "--remove-all",
+        action="store_true",
+        help=(
+            "Remove every guild-specific command only after confirming "
+            "the public commands exist globally."
+        )
+    )
     return parser.parse_args()
 
 
-async def remove_legacy_commands(guild_id: int):
+async def remove_legacy_commands(
+    guild_id: int,
+    remove_all: bool
+):
     intents = discord.Intents.none()
     client = discord.Client(intents=intents)
     tree = app_commands.CommandTree(client)
@@ -32,11 +44,29 @@ async def remove_legacy_commands(guild_id: int):
     async def on_ready():
         guild = discord.Object(id=guild_id)
         commands = await tree.fetch_commands(guild=guild)
-        obsolete = [
-            command
-            for command in commands
-            if command.name in OBSOLETE_COMMANDS
-        ]
+        if remove_all:
+            global_commands = await tree.fetch_commands()
+            global_names = {
+                command.name
+                for command in global_commands
+            }
+            missing = PUBLIC_GLOBAL_COMMANDS - global_names
+
+            if missing:
+                print(
+                    "Aborted: required global commands are missing: "
+                    + ", ".join(sorted(missing))
+                )
+                await client.close()
+                return
+
+            obsolete = commands
+        else:
+            obsolete = [
+                command
+                for command in commands
+                if command.name in OBSOLETE_COMMANDS
+            ]
 
         for command in obsolete:
             await command.delete()
@@ -62,7 +92,10 @@ def main():
     load_dotenv()
     arguments = parse_arguments()
     asyncio.run(
-        remove_legacy_commands(arguments.guild_id)
+        remove_legacy_commands(
+            arguments.guild_id,
+            arguments.remove_all
+        )
     )
 
 
