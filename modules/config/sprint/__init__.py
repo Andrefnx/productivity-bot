d809e0f2e1a20f45f7855b8d486250f1cca6b972
Andrea Henríquez <107231642+Andrefnx@users.noreply.github.com>
2026-08-29 02:55:40 -0300

    refactor: keep sprint UI out of sprint config package

--- modules/config/sprint/__init__.py
+++ modules/config/sprint/__init__.py
@@ -1,11 +1,12 @@
 from .sprint_config import (
-	DEFAULT_SPRINT_CONFIG,
-	create_sprint_config,
-	set_sprint_override
+    DEFAULT_SPRINT_CONFIG,
+    create_sprint_config,
+    set_sprint_override
 )
 
-from .sprint_views import (
-	SprintConfigView,
-	SprintSettingsView,
-	create_sprint_settings_embed
-)
+
+__all__ = [
+    "DEFAULT_SPRINT_CONFIG",
+    "create_sprint_config",
+    "set_sprint_override"
+]
