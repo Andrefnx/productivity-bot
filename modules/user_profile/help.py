7980f3dc0133ece8b0d6bc3fd8ce6bc594390b43
Andrea Henríquez <107231642+Andrefnx@users.noreply.github.com>
2026-08-29 02:50:28 -0300

    refactor: move profile help into profile module

--- modules/user_profile/help.py
+++ modules/user_profile/help.py
@@ -1,6 +1,19 @@
-from modules.help.help_messages import (
-    PROFILE_DESCRIPTION,
-    PROFILE_TITLE
+PROFILE_TITLE = "Profile"
+
+PROFILE_DESCRIPTION = (
+    "Use `/profile` to open your personal bot menu.\n\n"
+    "Your profile currently shows:\n"
+    "• Your name\n"
+    "• EXP information\n"
+    "• Your last project used in a sprint\n\n"
+    "From your profile you can open:\n"
+    "• My Projects\n"
+    "• Import JSON\n"
+    "• Settings\n\n"
+    "Your own profile menu is private to you.\n\n"
+    "Your privacy settings control whether other users will be "
+    "allowed to view your profile or projects when public profile "
+    "viewing is used."
 )
 
 
