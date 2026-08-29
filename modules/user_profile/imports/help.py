89966520292ef27325b80a745c377ef633ded197
Andrea Henríquez <107231642+Andrefnx@users.noreply.github.com>
2026-08-29 02:50:38 -0300

    refactor: move import help into import module

--- modules/user_profile/imports/help.py
+++ modules/user_profile/imports/help.py
@@ -1,6 +1,17 @@
-from modules.help.help_messages import (
-    IMPORTS_DESCRIPTION,
-    IMPORTS_TITLE
+IMPORTS_TITLE = "Importing Your Projects"
+
+IMPORTS_DESCRIPTION = (
+    "Already used another writing bot? You don't have to "
+    "create all your projects again.\n\n"
+    "**Importing from Writer Bot**\n\n"
+    "1. Open Writer Bot and export your data with `/export`.\n"
+    "2. Writer Bot will give you a `.json` file.\n"
+    "3. Open `/profile` here.\n"
+    "4. Press **Import JSON**.\n"
+    "5. Choose **Writer Bot** as the source.\n"
+    "6. Upload the file Writer Bot gave you.\n\n"
+    "Your projects and their saved information will be "
+    "added to your profile automatically."
 )
 
 
